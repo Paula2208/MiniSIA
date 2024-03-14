a5754ff43fadf1ad62d85b132411d13bd7964010
Paula Guzmán <pauladaguz@gmail.com>
2024-03-14 00:07:50 -0500

    First commit

---
+++ src/main/java/com/mycompany/minisia/MiniSIA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.minisia;
+
+/**
+ *
+ * @author paula
+ */
+public class MiniSIA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello! Mini SIA is begining");
+    }
+}
